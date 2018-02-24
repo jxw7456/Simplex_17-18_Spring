bdc234abffde8c2f3aec9e9b7a26d816287ef05d
jxw7456 <jxw7456@SND2536-01>
2018-02-24 14:41:33 -0500

    A03 started

--- A03 - LERP/AppClass.h
+++ A03 - LERP/AppClass.h
@@ -20,7 +20,9 @@ class Application
 {
 	uint m_uOrbits = 0; //number of shapes starting at 3 and increasing in sides
 	std::vector<uint> m_shapeList; //shape index for circles
-	String m_sProgrammer = "Alberto Bobadilla - labigm@rit.edu";
+	std::vector<std::vector<vector3>> orbits; // list of orbits
+	std::vector<uint> stops; // list of control variables for stops
+	String m_sProgrammer = "JaJuan Webster - jxw7456@rit.edu";
 	
 private:
 	static ImGuiObject gui; //GUI object
